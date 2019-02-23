8c5432b8efe169a24282550aa16802d0f653898c
wagnermarques <wagnerdocri@gmail.com>
2019-02-23 01:43:47 -0300

    ajuste no sql pra refletir dados da planilha provenientes do formsus

---
+++ res/create_db.sql
@@ -0,0 +1,38 @@
+create table polos(
+  id serial primary key,
+  nome varchar(50) not null
+);
+
+create table alunos(
+   id serial primary key,
+   nome varchar(100) not null,
+   dta_nasc date,
+   sexo varchar (10),
+   rg int (10),
+   escolaridade varchar(30),
+   cidade varchar(100),
+   estado varchar(10),
+   cep varchar(10),
+   endereco varchar(100),
+   bairro varchar(30),
+   celular varchar(13),
+   telefone varchar(13),
+   email varchar(50) not null,
+   profissao varchar(50),
+   ocupacao varchar(50),
+   local_trabalho varchar(100),
+   senha varchar(15) not null,
+   resp boolean not null,
+   fk_polo int not null,
+   foreign key (fk_polo) references polos(id)
+);
+
+
+
+create table frequencia(
+   id serial primary key,
+   data date not null,
+   presenca boolean not null,
+   fk_lista int not null,
+   foreign key (fk_lista) references lista(id)
+);
